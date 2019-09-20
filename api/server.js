874aa5f4c385b253e3ba1c02949b9154ec337c77
Duraan Ali <ali@sahalsolutions.com>
2019-09-20 11:07:10 -0500

    Finished setup and register functionality

--- api/server.js
+++ api/server.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+const session = require('express-session'); // <<<<<<<<<<<<<<<<<<<<<
+const KnexSessionStore = require('connect-session-knex')(session); // gotcha
+const jwt = require('jsonwebtoken');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const dbConnection = require('../database/dbConfig.js');
 
 const server = express();
+
+
+const sessionConfig = {
+    name: 'abdiwali', // would name the cookie sid by default
+    secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe',
+    cookie: {
+        maxAge: 1000 * 60 * 60, // in milliseconds
+        secure: false, // true means only send cookie over https
+        httpOnly: true, // true means JS has no access to the cookie
+    },
+    resave: false,
+    saveUninitialized: true, // GDPR compliance
+    store: new KnexSessionStore({
+        knex: dbConnection,
+        tablename: 'knexsessions',
+        sidfieldname: 'sessionid',
+        createtable: true,
+        clearInterval: 1000 * 60 * 30, // clean out expired session data
+    }),
+};
+
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
 const router = require('express').Router();
+const secrets = require('../config/secret.js');
+const bcrypt = require('bcryptjs');
+const Users = require('../auth/users-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let { username, password } = req.body;
+
+  const hash = bcrypt.hashSync(password, 8); // it's 2 ^ 8, not 8 rounds
+
+  Users.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        req.session.user = user; // <<<<<<<<<<<<<<<
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res.status(401).json({ message: 'You cannot pass!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // sub in payload is what the token is about
+    username: user.username,
+    // ...otherData
+  };
+
+  const options = {
+    expiresIn: '1d', // show other available options in the library's documentation
+  };
+
+  // extract the secret away so it can be required and used where needed
+  return jwt.sign(payload, secrets.jwtSecret, options); // this method is synchronous
+}
 
 module.exports = router;
